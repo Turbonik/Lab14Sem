dccf0046a83c61ba8131ebb2166dc1f0ea4f2cf9
User <User@DESKTOP-TKOB8QG>
2025-02-21 12:35:55 +0700

    Добавьте файлы проекта.

---
+++ lab1vage/FileHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1vage
+{
+    public static class FileHandler
+    {
+        //switch(int file_option){
+            
+        //    }
+    }
+}
